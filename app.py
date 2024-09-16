d9eb84b528e462ff5088ee1bda875b545282229b
h513h <h513h.h513h@gmail.com>
2024-09-16 17:18:03 +0900

    更新 app.py 並添加新的設定檔 (.gitignore, Procfile, railway.json, requirements.txt)

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "http://192.168.10.125:3000"]}})
+CORS(app)
 
-# 確保這些行在創建 SQLAlchemy 實例之前
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+# 使用環境變量或默認值設置數據庫 URI
+database_url = os.environ.get('DATABASE_URL', 'sqlite:///database.db')
+if database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -14,4 +19,5 @@ db = SQLAlchemy(app)
 from routes import *
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
